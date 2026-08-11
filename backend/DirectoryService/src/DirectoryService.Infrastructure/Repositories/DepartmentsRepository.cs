061dec3ab3545f15204bf2d5deb2eac0c625e0cd
Vlad Lakeev <chilli.willi.930@gmail.com>
2026-08-11 22:47:38 +0300

    fix

--- backend/DirectoryService/src/DirectoryService.Infrastructure/Repositories/DepartmentsRepository.cs
+++ backend/DirectoryService/src/DirectoryService.Infrastructure/Repositories/DepartmentsRepository.cs
@@ -59,8 +59,8 @@ public class DepartmentsRepository : IDepartmentsRepository
                 {
                     return GeneralErrors.AlreadyExists(nameof(Department), nameof(Department.Name), department.Name.Value);
                 }
+
                 if (pgEx.ConstraintName.Contains("slug", StringComparison.InvariantCultureIgnoreCase))
-                if (pgEx.ConstraintName.Contains("identifier", StringComparison.InvariantCultureIgnoreCase))
                 {
                     return GeneralErrors.AlreadyExists(nameof(Department), nameof(Department.Slug), department.Slug.Value);
                 }
