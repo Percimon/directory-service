a9481f186e9b6bc93cd4246c7e8bdba0ef881a6a
Vlad Lakeev <chilli.willi.930@gmail.com>
2026-08-19 16:43:53 +0300

    fix

--- backend/SharedService/SharedService.Framework/EndpointResults/ErrorsResult.cs
+++ backend/SharedService/SharedService.Framework/EndpointResults/ErrorsResult.cs
@@ -31,7 +31,7 @@ public sealed class ErrorsResult : IResult
             ErrorType.VALIDATION => StatusCodes.Status400BadRequest,
             ErrorType.NOT_FOUND => StatusCodes.Status404NotFound,
             ErrorType.CONFLICT => StatusCodes.Status409Conflict,
-            ErrorType.FAILURE => StatusCodes.Status400BadRequest,
+            ErrorType.FAILURE => StatusCodes.Status500InternalServerError,
             ErrorType.AUTHENTICATION => StatusCodes.Status401Unauthorized,
             ErrorType.AUTHORIZATION => StatusCodes.Status403Forbidden,
             _ => StatusCodes.Status500InternalServerError,
